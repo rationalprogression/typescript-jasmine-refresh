a9aab66c3424ad9546d90846b2f6c557bb8273ab
Roby Van Damme <roby@robyvandamme.com>
2017-10-20 08:45:12 +0700

    Update gulp tasks: separate test and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
-var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
+const gulp = require('gulp');
+const jasmine = require('gulp-jasmine');
+const reporters = require('jasmine-reporters');
+const ts = require('gulp-typescript');
 
-gulp.task('test', () =>
+// var tsProject = ts.createProject("tsconfig.json", { compileOnSave: true });
+
+// gulp.task('build', () => {
+//     build();
+// })
+
+gulp.task('test', () => {
+    test();
+});
+
+gulp.task('watch', () => {
+    test();
+    watch();
+});
+
+function test() {
+    gulp.src('js/spec/**/*Spec.js')
+    // gulp-jasmine works on filepaths so you can't have any plugins before it
+    .pipe(jasmine({
+        verbose: true,
+        errorOnFail: false,
+        // reporter: new reporters.NUnitXmlReporter()
+    })
+    .on('error', function(err) {
+        // catch errors to prevent the watch from stopping
+      }));
+}
+
+function watch(){
     gulp.watch(['js/src/**/*.js', 'js/spec/**/*Spec.js'], () =>
     gulp.src('js/spec/**/*Spec.js')
-        // gulp-jasmine works on filepaths so you can't have any plugins before it
-        .pipe(jasmine())
-        .on('error', function(err) {
-            // catch errors to prevent the watch from stopping
-          })
-));
+    // gulp-jasmine works on filepaths so you can't have any plugins before it
+    .pipe(jasmine({
+        verbose: true,
+        errorOnFail: false
+    }))
+    .on('error', function(err) {
+        // catch errors to prevent the watch from stopping
+      }));
+}
